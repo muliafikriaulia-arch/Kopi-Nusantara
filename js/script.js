1bf45babb1564fe54ef39161ad6a2db99ffd1377
muliafikriaulia-arch <muliafikriaulia@gmail.com>
2026-09-07 20:14:44 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const promoButton = document.querySelector("#promoButton");
+
+promoButton.addEventListener("click", ()=>{
+    promoButton.textContent="promo: beli 2 gratis testes!";
+    console.log("promo kopi nusantara berhasil ditampilkan");
+
+})
